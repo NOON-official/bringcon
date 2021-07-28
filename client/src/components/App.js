e420e6b4473f068716a4b040b1a359f3ac9a45bb
Minjeong Kim <70756727+minjeong-kim-git@users.noreply.github.com>
2021-07-28 14:40:54 +0900

    Update App.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -13,6 +13,8 @@ import HashTagPage from "./views/LandingPage/HashTagPage";
 import CartPage from "./views/CartPage/CartPage";
 import HistoryPage from "./views/HistoryPage/HistoryPage";
 import CommunicationChat from "./views/Chatbot/Chat";
+import BodyPage from './views/Board/BodyPage';
+import BoardWriteForm from './views/Board/BoardWriteForm';
 
 //null   Anyone Can go inside
 //true   only logged in user can go inside
@@ -44,6 +46,11 @@ function App() {
           />
           <Route exact path="/user/cart" component={Auth(CartPage, true)} />
           <Route exact path="/history" component={Auth(HistoryPage, true)} />
+          <Route
+            exact
+            path="/write"
+            component={Auth(BodyPage, true)}/>
+            <Route path="/boardWrite" component={Auth(BoardWriteForm, true)}></Route>
         </Switch>
         <CommunicationChat/>
       </div>

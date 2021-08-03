03e6e998c13ec02691d27193d6dfa3b0ed854aa1
Moon Gyuwon <s201801402@hufs-gsuite.kr>
2021-08-03 10:56:00 +0900

    edit README.md & config

---
+++ ecosystem.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+    apps: [
+        {
+            name: "client",
+            script: "npm run frontend-prod",
+            
+            env: {
+                "PORT": 3000,
+                "NODE_ENV": "development"
+            },
+           
+            env_production: {
+                "PORT": 80,
+                "NODE_ENV": "production"
+            }
+        },{
+            name      : "server",
+            script    : "npm run backend",
+            
+            env: {
+                "PORT": 5000,
+                "NODE_ENV": "development"
+            },
+        
+            env_production: {
+                "PORT": 5000,
+                "NODE_ENV": "production"
+            }
+        }
+    ]
+};

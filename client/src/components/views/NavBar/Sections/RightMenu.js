1b8fa9ee33fa7b349683f87cd5413744feb9e78a
Minjeong Kim <70756727+minjeong-kim-git@users.noreply.github.com>
2021-08-12 14:25:02 +0900

    Add files via upload

--- client/src/components/views/NavBar/Sections/RightMenu.js
+++ client/src/components/views/NavBar/Sections/RightMenu.js
@@ -43,21 +43,24 @@ function RightMenu(props) {
         <Menu.Item key="upload">
           <a href="/product/upload" style={{color: "#ffcb39"}}>Upload</a>
         </Menu.Item>
+        <Menu.Item key="cart">
+          <a href="/user/cart" style={{color: "#ffcb39"}}>Cart</a>
+        </Menu.Item>
 
-        <Menu.Item key="cart" style={{ paddingBottom: 3 }}>
+        {/* <Menu.Item key="cart">
           <Badge count={user.userData && user.userData.cart.length} style={{zIndex: 10}}>
             <a
               href="/user/cart"
               className="head-example"
-              style={{ marginRight: -22, color: "#667777"}}
-            >
-              <Icon
+              style={{color: "#ffcb39"}}
+            >Cart */}
+              {/* <Icon
                 type="shopping-cart"
                 style={{ fontSize: 30, marginBottom: 3 }}
-              />
-            </a>
+              /> */}
+            {/* </a>
           </Badge>
-        </Menu.Item>
+        </Menu.Item> */}
 
         <Menu.Item key="logout">
           <a onClick={logoutHandler} style={{color: "#ffcb39"}}>Log out</a>

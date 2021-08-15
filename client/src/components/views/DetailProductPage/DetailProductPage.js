6c33a7631b86dd60d534fcd1ffa3284671827aa2
YeonSeong-Lee <62806979+YeonSeong-Lee@users.noreply.github.com>
2021-08-16 04:05:21 +0900

    [fix] addtocart
    addtocart 버튼을 눌러도 카트에 추가 안되는 버그 발생 
    
    prop.detail._id 를 읽어오지 못해서 생긴 문제. (DetailProductPage의 prop에는 detail요소가 없음.)
    
    대신 쿼리로 받아온 id값을 넣어서 해결함.

--- client/src/components/views/DetailProductPage/DetailProductPage.js
+++ client/src/components/views/DetailProductPage/DetailProductPage.js
@@ -4,11 +4,11 @@ import Tabs from "./Sections/Tabs";
 import { Row, Col } from "antd";
 import { useDispatch } from "react-redux";
 import { addToCart } from "../../../_actions/user_actions";
-import Ratio from 'react-ratio';
-import styled from 'styled-components';
+import Ratio from "react-ratio";
+import styled from "styled-components";
 import { Button } from "antd";
-import Swal from 'sweetalert2';
-import './css/DetailPage.css'
+import Swal from "sweetalert2";
+import "./css/DetailPage.css";
 
 function DetailProductPage(props) {
   const dispatch = useDispatch();
@@ -16,11 +16,11 @@ function DetailProductPage(props) {
   const [Product, setProduct] = useState({});
 
   const DetailTemplate = styled.div`
-  video {
-    width: 100%;
-    height: 100%;
-  }
-`;
+    video {
+      width: 100%;
+      height: 100%;
+    }
+  `;
 
   useEffect(() => {
     axios
@@ -32,14 +32,9 @@ function DetailProductPage(props) {
   }, [productId]);
 
   const clickHandler = () => {
-    
     //필요한 정보를 Cart 필드에다가 넣어 준다.
-    dispatch(addToCart(props.detail._id));
-    Swal.fire(
-      'Success!',
-      '카트에 상품이 추가되었습니다.',
-      'success'
-    )
+    dispatch(addToCart(productId));
+    Swal.fire("Success!", "카트에 상품이 추가되었습니다.", "success");
   };
 
   return (
@@ -48,42 +43,56 @@ function DetailProductPage(props) {
         <Col lg={12} sm={24}>
           {/* ProductVideo */}
           <DetailTemplate>
-          <Ratio ratio={ 16 / 9 }>
-          <video
-            style={{backgroundColor: "black", borderRadius: '12px'}}
-            src={`${Product.filePath}`}
-            controls
-            controlsList="nodownload"
-          />
-          </Ratio>
-        </DetailTemplate>
-        <div style={{ display: "flex", justifyContent: "flex-start", paddingTop: '20px'}}>
-        <h1>{Product.title}</h1>
-        <div style={{ display: "flex", justifyContent: "center" }}>
-        
-      </div>
-      </div>
-      {/* <div id="tags">
+            <Ratio ratio={16 / 9}>
+              <video
+                style={{ backgroundColor: "black", borderRadius: "12px" }}
+                src={`${Product.filePath}`}
+                controls
+                controlsList="nodownload"
+              />
+            </Ratio>
+          </DetailTemplate>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "flex-start",
+              paddingTop: "20px",
+            }}
+          >
+            <h1>{Product.title}</h1>
+            <div style={{ display: "flex", justifyContent: "center" }}></div>
+          </div>
+          {/* <div id="tags">
       <span><a href={`/hashtag/${Product.tags}`}>#{Product.tags}</a></span>
       </div> */}
-      <div id="info">
-        <span id="price">가격: {Product.price}</span>
-        <span id="sold">sold: {Product.sold}</span>
-        <span id="view">view: {Product.views}</span>
-      </div>
-      <Button class="addtocart" size="large" shape="round" onClick={clickHandler}>
-          여행 목록에 담기
-        </Button>
-        <Button class="addtocart" size="large" shape="round" onClick={clickHandler}>
-          Add to Cart
-        </Button>
-      </Col>
+          <div id="info">
+            <span id="price">가격: {Product.price}</span>
+            <span id="sold">sold: {Product.sold}</span>
+            <span id="view">view: {Product.views}</span>
+          </div>
+          <Button
+            class="addtocart"
+            size="large"
+            shape="round"
+            onClick={clickHandler}
+          >
+            여행 목록에 담기
+          </Button>
+          <Button
+            class="addtocart"
+            size="large"
+            shape="round"
+            onClick={clickHandler}
+          >
+            Add to Cart
+          </Button>
+        </Col>
         {/* <Col lg={12} sm={24}> */}
-          {/* ProductInfo */}
-          {/* <ProductInfo detail={Product} /> */}
+        {/* ProductInfo */}
+        {/* <ProductInfo detail={Product} /> */}
         {/* </Col> */}
         <Col lg={12} sm={24}>
-          <Tabs detail={Product}/>
+          <Tabs detail={Product} />
         </Col>
       </Row>
     </div>

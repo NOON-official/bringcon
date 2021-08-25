4b5ba05e1a16fb68b2af0e33818219619ead8852
YeonSeong-Lee <dltjddus3@gmail.com>
2021-08-25 13:45:08 +0900

    [add] 선호 태그 등록 페이지

--- client/src/components/views/LoginPage/WelcomePage.js
+++ client/src/components/views/LoginPage/WelcomePage.js
@@ -3,10 +3,27 @@ import { withRouter } from "react-router-dom";
 import SPLASH from "../../utils/SPLASH.gif";
 
 function WelcomePage(props) {
-  console.log(props);
-  setTimeout(() => {
-    props.history.push("/");
-  }, 3000);
+  let hasInterests = false;
+
+  if (
+    props.user.userData &&
+    props.user.userData.interests &&
+    props.user.userData.interests.length > 0
+  ) {
+    console.log(props.user.userData.interests.length);
+    hasInterests = true;
+  }
+
+  if (hasInterests) {
+    setTimeout(() => {
+      props.history.push("/");
+    }, 3000);
+  } else {
+    setTimeout(() => {
+      props.history.push("/user/interests");
+    }, 3000);
+  }
+
   return (
     <div>
       <img style={{ height: "100%", width: "100%" }} src={SPLASH}></img>
---
+++ client/src/components/views/MyPage/Sections/AddInterests.js
@@ -0,0 +1,99 @@
+import React, { useState } from "react";
+import Axios from "axios";
+import { useSelector } from "react-redux";
+import { Checkbox, Row, Col, Form } from "antd";
+const Options = [
+  "동물",
+  "음식",
+  "클립영상",
+  "유머",
+  "게임",
+  "애니메이션",
+  "일상",
+  "음악",
+  "뉴스",
+  "영화 및 TV",
+];
+
+function AddInterests(props) {
+  const user = useSelector((state) => state.user);
+  const [checkedList, setCheckedList] = useState([]);
+  const onChange = (list) => {
+    setCheckedList(list);
+    console.log("list", list);
+  };
+  console.log("chkeckedList", checkedList);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    if (checkedList.length < 1) {
+      return alert("하나 이상을 선택해주세요. ");
+    }
+
+    //서버에 채운 값들을 request로 보낸다.
+
+    const body = {
+      //로그인 된 사람의 ID
+      _id: user.userData._id,
+      interests: checkedList,
+    };
+
+    Axios.post("/api/users/interests", body).then((response) => {
+      if (response.data.success) {
+        alert("선호 태그 등록 완료");
+        props.history.push("/");
+      } else {
+        alert("등록에 실패하였습니다. 관리자에게 문의하세요.");
+      }
+    });
+  };
+
+  return (
+    <div style={{ maxWidth: "1000px", margin: "2rem auto" }}>
+      <div style={{ textAlign: "center", marginBottom: "2rem" }}>
+        <h2> 선호하는 키워드를 모두 선택하세요! </h2>
+      </div>
+      <Form onSubmit={submitHandler}>
+        <Checkbox.Group onChange={onChange}>
+          <Row>
+            <Col span={4}>
+              <Checkbox value="동물">동물</Checkbox>
+            </Col>
+            <Col span={4}>
+              <Checkbox value="음식">음식</Checkbox>
+            </Col>
+            <Col span={4}>
+              <Checkbox value="클립영상">클립영상</Checkbox>
+            </Col>
+            <Col span={4}>
+              <Checkbox value="유머">유머</Checkbox>
+            </Col>
+            <Col span={4}>
+              <Checkbox value="게임">게임</Checkbox>
+            </Col>
+          </Row>
+          <Row>
+            <Col span={4}>
+              <Checkbox value="애니메이션">애니메이션</Checkbox>
+            </Col>
+            <Col span={4}>
+              <Checkbox value="일상">일상</Checkbox>
+            </Col>
+            <Col span={4}>
+              <Checkbox value="음악">음악</Checkbox>
+            </Col>
+            <Col span={4}>
+              <Checkbox value="뉴스">뉴스</Checkbox>
+            </Col>
+            <Col span={4}>
+              <Checkbox value="영화 및 TV">영화 및 TV</Checkbox>
+            </Col>
+          </Row>
+        </Checkbox.Group>
+        <button type="submit">여행 시작하기</button>
+      </Form>
+    </div>
+  );
+}
+
+export default AddInterests;

c0b80a79c3a9d6e58ab6a88125da215a2913681a
Minjeong Kim <70756727+minjeong-kim-git@users.noreply.github.com>
2021-08-27 10:45:09 +0900

    Add files via upload

--- client/src/components/views/MyPage/Sections/UpdateUserInfo.js
+++ client/src/components/views/MyPage/Sections/UpdateUserInfo.js
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
-import { Form, Input } from "antd";
+import { Form, Input, Col } from "antd";
 import Axios from "axios";
 import { useSelector } from "react-redux";
+import VerticalMenu from '../../VerticalMenu/VerticalMenu';
+
 const { TextArea } = Input;
 
 function UpdateUserInfo(props) {
   const user = useSelector((state) => state.user);
   const [Name, setName] = useState("");
+  const [toggleState, setToggleState] = useState(1);
 
   const NameChangeHandler = (event) => {
     setName(event.currentTarget.value);
@@ -36,12 +39,29 @@ function UpdateUserInfo(props) {
     });
   };
 
-  return (
-    <div style={{ maxWidth: "1000px", margin: "2rem auto" }}>
-      <div style={{ textAlign: "center", marginBottom: "2rem" }}>
-        <h2> 회원 정보 수정 </h2>
-      </div>
+  const toggleTab = (index) => {
+    setToggleState(index);
+  };
 
+  return (
+    <div id="body" style={{paddingTop: '50px', width: 'auto', margin: 'auto'}}>
+      <Col style={{float: 'left', marginLeft: '84px', marginRight: '84px'}}>
+        <VerticalMenu/>
+      </Col>
+      <Col style={{float: 'right'}}>
+      <div className="mypage-container">
+        <div className="mypage-bloc-tabs">
+          <button
+            className={toggleState === 1 ? "mypage-tabs active-tabs" : "mypage-tabs"}
+            onClick={() => toggleTab(1)}
+          >
+            기본 정보
+          </button>
+        </div>
+        <div className="mypage-content-tabs">
+        <div
+          className={toggleState === 1 ? "mypage-content  active-content" : "mypage-content"}
+        >
       <Form onSubmit={submitHandler}>
         <div style={{ justifyContent: "space-between" }}>
           <label>이름</label>
@@ -50,6 +70,10 @@ function UpdateUserInfo(props) {
         </div>
         <button type="submit">제출</button>
       </Form>
+      </div>
+      </div>
+    </div>
+    </Col>
     </div>
   );
 }

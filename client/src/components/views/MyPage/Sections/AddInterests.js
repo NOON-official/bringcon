7601a199e64c6acc4626349ee3f61780236ee657
YeonSeong-Lee <dltjddus3@gmail.com>
2021-08-30 21:16:26 +0900

    [fix] AddInterests.js, checklist가 제대로 들어가게

--- client/src/components/views/MyPage/Sections/AddInterests.js
+++ client/src/components/views/MyPage/Sections/AddInterests.js
@@ -2,8 +2,8 @@ import React, { useState } from "react";
 import Axios from "axios";
 import { useSelector } from "react-redux";
 import { Form } from "antd";
-import '../css/UserInfo.css';
-import Swal from 'sweetalert2';
+import "../css/UserInfo.css";
+import Swal from "sweetalert2";
 
 const Options = [
   "동물",
@@ -20,16 +20,21 @@ const Options = [
 
 function AddInterests(props) {
   const user = useSelector((state) => state.user);
-  const [checkedList, setCheckedList] = useState([]);
-  const onChange = (list) => {
-    setCheckedList(list);
-    console.log("list", list);
-  };
-  console.log("chkeckedList", checkedList);
 
+  const [CheckList, setCheckList] = useState([]);
+  console.log(CheckList);
+  const onChangeEach = (e, id) => {
+    // 체크할 시 CheckList에 id값 넣기
+    if (e.target.checked) {
+      setCheckList([...CheckList, id]);
+      // 체크 해제할 시 CheckList에서 해당 id값이 `아닌` 값만 배열에 넣기
+    } else {
+      setCheckList(CheckList.filter((checkedId) => checkedId !== id));
+    }
+  };
   const submitHandler = (event) => {
     event.preventDefault();
-    if (checkedList.length < 1) {
+    if (CheckList.length < 1) {
       return alert("하나 이상을 선택해주세요. ");
     }
 
@@ -38,19 +43,21 @@ function AddInterests(props) {
     const body = {
       //로그인 된 사람의 ID
       _id: user.userData._id,
-      interests: checkedList,
+      interests: CheckList,
     };
 
     Axios.post("/api/users/interests", body).then((response) => {
       if (response.data.success) {
         Swal.fire({
-          title: 'Success!',
-          text: '내 콘텐츠 행성에 여행객을 맞을 준비를 마쳤습니다.',
-          imageUrl: 'https://s3.us-west-2.amazonaws.com/secure.notion-static.com/d5604f65-07b8-427a-8a3c-8ee5ef0dc5b0/pop-up3.svg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAT73L2G45O3KS52Y5%2F20210830%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20210830T042501Z&X-Amz-Expires=86400&X-Amz-Signature=d6ee907acc00966b5abb379254efde0cac3f69d3c195346958e3d497874740c2&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22pop-up3.svg%22',
+          title: "Success!",
+          text: "내 콘텐츠 행성에 여행객을 맞을 준비를 마쳤습니다.",
+          imageUrl:
+            "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/d5604f65-07b8-427a-8a3c-8ee5ef0dc5b0/pop-up3.svg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAT73L2G45O3KS52Y5%2F20210830%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20210830T042501Z&X-Amz-Expires=86400&X-Amz-Signature=d6ee907acc00966b5abb379254efde0cac3f69d3c195346958e3d497874740c2&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22pop-up3.svg%22",
           imageWidth: 200,
           imageHeight: 176,
-          imageAlt: 'Custom image',
-          background: '#fff url(https://s3.us-west-2.amazonaws.com/secure.notion-static.com/b2513ed5-eab8-4626-8a07-e788d7d9952e/BACK_star%28trans%29.svg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAT73L2G45O3KS52Y5%2F20210830%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20210830T044712Z&X-Amz-Expires=86400&X-Amz-Signature=6664bf9459c6e1d4115105918d83a1312ecb9ac22d9e751bc8c2b4b63321ee20&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22BACK_star%28trans%29.svg%22)'
+          imageAlt: "Custom image",
+          background:
+            "#fff url(https://s3.us-west-2.amazonaws.com/secure.notion-static.com/b2513ed5-eab8-4626-8a07-e788d7d9952e/BACK_star%28trans%29.svg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAT73L2G45O3KS52Y5%2F20210830%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20210830T044712Z&X-Amz-Expires=86400&X-Amz-Signature=6664bf9459c6e1d4115105918d83a1312ecb9ac22d9e751bc8c2b4b63321ee20&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22BACK_star%28trans%29.svg%22)",
         });
         props.history.push("/");
       } else {
@@ -60,79 +67,48 @@ function AddInterests(props) {
   };
 
   return (
-    <div id="body" style={{width: 'auto'}}>
-    <div style={{width: '90%', backgroundColor: '#1C1C1C', margin: 'auto', paddingTop: '50px'}}>
-      <div className="preference" style={{textAlign: "center", marginBottom: "2rem", margin: 'auto'}}>
-        선호하는 키워드를 모두 선택하세요!
+    <div id="body" style={{ width: "auto" }}>
+      <div
+        style={{
+          width: "90%",
+          backgroundColor: "#1C1C1C",
+          margin: "auto",
+          paddingTop: "50px",
+        }}
+      >
+        <div
+          className="preference"
+          style={{ textAlign: "center", marginBottom: "2rem", margin: "auto" }}
+        >
+          선호하는 키워드를 모두 선택하세요!
+        </div>
+        <Form onSubmit={submitHandler}>
+          <div
+            className="interest-row"
+            style={{ width: "75%", margin: "auto" }}
+          >
+            {Options.map((option, index) => {
+              return (
+                <label className="interest-label" key={index}>
+                  <input
+                    type="checkbox"
+                    className="interest-check"
+                    onChange={(e) => onChangeEach(e, Options[index])}
+                  />
+                  <div className="interest-box">
+                    <span>{option}</span>
+                  </div>
+                </label>
+              );
+            })}
+            <div className="button-cell">
+              <button type="submit" className="set-tags-button">
+                여행 시작하기
+              </button>
+            </div>
+          </div>
+        </Form>
       </div>
-      <Form onSubmit={submitHandler}>
-        <div className="interest-row" style={{width: '75%', margin: 'auto'}} onChange={onChange}>
-        <label className="interest-label">
-          <input type="checkbox" className="interest-check" value="동물" />
-          <div className="interest-box">
-            <span>동물</span>
-          </div>
-        </label>
-        <label className="interest-label">
-          <input type="checkbox" className="interest-check" value="음식"/>
-          <div className="interest-box">
-            <span>음식</span>
-          </div>
-        </label>
-        <label className="interest-label">
-          <input type="checkbox" className="interest-check" value="애니메이션"/>
-          <div className="interest-box">
-            <span>애니메이션</span>
-          </div>
-        </label>
-        <label className="interest-label">
-          <input type="checkbox" className="interest-check" value="일상"/>
-          <div className="interest-box">
-            <span>일상</span>
-          </div>
-        </label>
-        <label className="interest-label">
-          <input type="checkbox" className="interest-check" value="게임"/>
-          <div className="interest-box">
-            <span>게임</span>
-          </div>
-        </label>
-        <label className="interest-label">
-          <input type="checkbox" className="interest-check" value="클립영상"/>
-          <div className="interest-box">
-            <span>클립영상</span>
-          </div>
-        </label>
-        <label className="interest-label">
-          <input type="checkbox" className="interest-check" value="음악"/>
-          <div className="interest-box">
-            <span>음악</span>
-          </div>
-        </label>
-        <label className="interest-label">
-          <input type="checkbox" className="interest-check" value="유머"/>
-          <div className="interest-box">
-            <span>유머</span>
-          </div>
-        </label>
-        <label className="interest-label">
-          <input type="checkbox" className="interest-check" value="뉴스"/>
-          <div className="interest-box">
-            <span>뉴스</span>
-          </div>
-        </label>
-        <label className="interest-label">
-          <input type="checkbox" className="interest-check" value="영화 및 TV"/>
-          <div className="interest-box">
-            <span>영화 및 TV</span>
-          </div>
-        </label>
-        <div className="button-cell">
-          <button type="submit" className="set-tags-button">여행 시작하기</button>
-        </div>
-        </div>
-      </Form>
-    </div>
     </div>
   );
 }

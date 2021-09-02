f0ed699c7dc74653fed94520e5a94073c63e4618
Redcated <skcjfdnd1996@naver.com>
2021-09-02 13:44:17 +0900

    Commented Redirect cuz of error

--- client/src/components/views/Main/MainPage.js
+++ client/src/components/views/Main/MainPage.js
@@ -5,19 +5,19 @@ import ufo from '../LoginPage/sections/ufo.svg';
 
 function MainPage(){
     
-    const submitSearchTerm = (newSearchTerm) => {
+    /*const submitSearchTerm = (newSearchTerm) => {
         return ( <Redirect to={{
             pathname: '/contents',
             state: { SearchTerm: newSearchTerm }
         }}/> );
-    }
+    }*/
     
     return(
         <div id="body" style={{width: '100vw'}}>
             <div className="main-info">
                 <h2>광활한 콘텐츠의 우주에 오신 것을 환영합니다.<br/>
                     저는 당신의 콘텐츠 여행을 돕는 가이드, BRINGCON입니다.</h2>
-                <SearchFeature refreshFunction={submitSearchTerm} style={{width: '500px'}}/>
+                <SearchFeature /*refreshFunction={submitSearchTerm}*/ style={{width: '500px'}}/>
             </div>
             <div className="decorations">
                 <img src={ufo} className="ufo"/>

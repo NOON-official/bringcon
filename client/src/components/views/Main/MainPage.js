30130f824f655e413965dc4f9bbbb84916c189cd
Redcated <skcjfdnd1996@naver.com>
2021-09-07 16:28:24 +0900

    [test] main page refresh function added

--- client/src/components/views/Main/MainPage.js
+++ client/src/components/views/Main/MainPage.js
@@ -1,4 +1,5 @@
-import React, { Redirect } from 'react';
+import React from 'react';
+import { Redirect } from 'react-router-dom';
 import SearchFeature from '../LandingPage/Sections/SearchFeature';
 import './Main.css';
 import ufo from '../LoginPage/sections/ufo.svg';
@@ -17,7 +18,7 @@ function MainPage(){
             <div className="main-info">
                 <h2>광활한 콘텐츠의 우주에 오신 것을 환영합니다.<br/>
                     저는 당신의 콘텐츠 여행을 돕는 가이드, BRINGCON입니다.</h2>
-                <SearchFeature /*refreshFunction={submitSearchTerm}*/ style={{width: '500px'}}/>
+                <SearchFeature refreshFunction={submitSearchTerm} style={{width: '500px'}}/>
             </div>
             <div className="decorations">
                 <img src={ufo} className="ufo"/>

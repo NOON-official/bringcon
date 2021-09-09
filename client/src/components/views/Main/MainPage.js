4ff2454f666ca3517cac028cca4841abea131e31
Redcated <skcjfdnd1996@naver.com>
2021-09-09 14:11:32 +0900

    Added Google Search Tag (SEO)

--- client/src/components/views/Main/MainPage.js
+++ client/src/components/views/Main/MainPage.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 import SearchFeature from '../LandingPage/Sections/SearchFeature';
 import './Main.css';
 import ufo from '../LoginPage/sections/ufo.svg';
@@ -17,6 +18,9 @@ function MainPage(){
     
     return(
         <div id="body" style={{width: '100vw'}}>
+            <Helmet>
+                < meta name="google-site-verification" content="7n5xoSLEUqYqdIAzcqRHF0y5c4Q-A-FFJC4_BsDC9TQ" /> 
+            </Helmet>
             <div className="main-info">
                 <h2>광활한 콘텐츠의 우주에 오신 것을 환영합니다.<br/>
                     저는 당신의 콘텐츠 여행을 돕는 가이드, BRINGCON입니다.</h2>

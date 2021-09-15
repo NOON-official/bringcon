308934bfd5bfda999448036ad3ce6edb6ac1e3d4
minjeong <fran0666@naver.com>
2021-09-15 12:59:31 +0900

    Reversed Order in MyContentPage

--- client/src/components/views/MyPage/ReviewPage/ReviewPage.js
+++ client/src/components/views/MyPage/ReviewPage/ReviewPage.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import {Col, Comment, Avatar} from'antd'; 
 import VerticalMenu from '../VerticalMenu/VerticalMenu';
 import './ReviewPage.css';
+import mobile from '../../Main/mobile.png';
 import Swal from "sweetalert2";
 import Cry from '../../../utils/Cry.svg';
 import Success from '../../../utils/Success.svg';
@@ -46,13 +47,24 @@ function ReviewPage(props) {
                         if (response.data.success) {
                             setReviews(Reviews => [...Reviews, { review: review, product: response.data.product[0] }])
                         } else {
-                            
-                            alert("후기 상품 정보를 가져오는데 실패했습니다.")
+                            Swal.fire({
+                                title: 'Oops...!',
+                                text: '후기 상품 정보를 가져오는 데 실패했습니다.',
+                                imageUrl: Error,
+                                imageWidth: 200,
+                                imageHeight: 176
+                            })
                         }
                     })
                 })
             } else {
-                alert("후기를 가져오는데 실패했습니다.");
+                Swal.fire({
+                    title: 'Oops...!',
+                    text: '후기를 가져오는 데 실패했습니다.',
+                    imageUrl: Error,
+                    imageWidth: 200,
+                    imageHeight: 176
+                })
             }
         })
     }
@@ -108,70 +120,75 @@ function ReviewPage(props) {
     };
 
     return (
-    <div id="body" style={{paddingTop: '50px', maxWidth: '100vw', margin: 'auto'}}>
-        <Col style={{float: 'left', marginLeft: '84px', marginRight: 0}}>
-            <VerticalMenu/>
-        </Col>
-        <Col style={{float: 'right', width: '1150px'}}>
-            <div className="reviews-container">
-                <div className="mypage-bloc-tabs">
-                    <button className={toggleState === 1 ? "mypage-tabs active-tabs" : "mypage-tabs"}
-                    onClick={() => toggleTab(1)}>
-                        내 후기
-                    </button>
-                    {/* <SearchFeature/> */}
-                </div>
-                <div className="review-list">
-                    <table style={{width: '900px', margin: 'auto'}}>
-                        {props.user.userData && Reviews.map((review, index) => (
-                            <tbody className="review-tbody" key={index} style={{width: '900px', margin: 'auto'}}>
-                                <tr className="review-row" style={{height: '120px'}}>
-                                    <td style={{width: '200px'}}>
-                                        {/* 상품 썸네일 */}
-                                        <img
-                                            style={{ width: "142px", height: "80px", borderRadius: "8px" }}
-                                            alt="review"
-                                            src={review.product.s3thumbnail}
-                                        />
-                                    </td>
-                                    <td style={{width: '145px'}}>
-                                        {/* 상품 제목 */}
-                                        <div className="review-title">{review.product.title}</div>
-                                        {/* 상품 올린 사람 */}
-                                        <div className="review-writer">{review.product.writer.name}</div>
-                                    </td>
-                                    <td>
-                                        {/* 후기 */}
-                                        <div className="comment">
-                                            <Comment
-                                                author={review.review.writer.name}
-                                                avatar = {
-                                                    <Avatar
-                                                        style={{width:"42px", height:"42px"}}
-                                                        src={review.review.writer && review.review.writer.image}
-                                                        alt="image"/>
-                                                }
-                                                content = {
-                                                    <p id="review-content" className="comment-content">
-                                                        {review.review.content}
-                                                    </p>
-                                                }
-                                            />
-                                        </div>
-                                    </td>
-                                    <td>
-                                        {/* 삭제 & 수정 버튼 */}
-                                        <button className="delete-button" onClick={e => { e.preventDefault(); handleDelete(review.review._id)} }>삭제</button>
-                                        <br/>
-                                        <button className="edit-button" onClick={e => { e.preventDefault(); handleEdit(review.review._id)} }>수정</button>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        ))}
-                    </table>
-                </div>
+        <div>
+            <div id="small-body">
+                <img src={mobile} className="mobile"/>
             </div>
-        </Col>
+            <div id="body" style={{paddingTop: '50px', maxWidth: '100vw', margin: 'auto'}}>
+                <Col style={{float: 'left', marginLeft: '84px', marginRight: 0}}>
+                    <VerticalMenu/>
+                </Col>
+                <Col style={{float: 'right', width: '1150px'}}>
+                    <div className="reviews-container">
+                        <div className="mypage-bloc-tabs">
+                            <button className={toggleState === 1 ? "mypage-tabs active-tabs" : "mypage-tabs"}
+                            onClick={() => toggleTab(1)}>
+                                내 후기
+                            </button>
+                        {/* <SearchFeature/> */}
+                        </div>
+                        <div className="review-list">
+                            <table style={{width: '900px', margin: 'auto'}}>
+                                {props.user.userData && Reviews.map((review, index) => (
+                                <tbody className="review-tbody" key={index} style={{width: '900px', margin: 'auto'}}>
+                                    <tr className="review-row" style={{height: '120px'}}>
+                                        <td style={{width: '200px'}}>
+                                            {/* 상품 썸네일 */}
+                                            <img
+                                                style={{ width: "142px", height: "80px", borderRadius: "8px" }}
+                                                alt="review"
+                                                src={review.product.s3thumbnail}
+                                            />
+                                        </td>
+                                        <td style={{width: '145px'}}>
+                                            {/* 상품 제목 */}
+                                            <div className="review-title">{review.product.title}</div>
+                                            {/* 상품 올린 사람 */}
+                                            <div className="review-writer">{review.product.writer.name}</div>
+                                        </td>
+                                        <td>
+                                            {/* 후기 */}
+                                            <div className="comment">
+                                                <Comment
+                                                    author={review.review.writer.name}
+                                                    avatar = {
+                                                        <Avatar
+                                                            style={{width:"42px", height:"42px"}}
+                                                            src={review.review.writer && review.review.writer.image}
+                                                            alt="image"/>
+                                                        }
+                                                    content = {
+                                                        <p id="review-content" className="comment-content">
+                                                            {review.review.content}
+                                                        </p>
+                                                    }
+                                                />
+                                            </div>
+                                        </td>
+                                        <td>
+                                            {/* 삭제 & 수정 버튼 */}
+                                            <button className="delete-button" onClick={e => { e.preventDefault(); handleDelete(review.review._id)} }>삭제</button>
+                                            <br/>
+                                            <button className="edit-button" onClick={e => { e.preventDefault(); handleEdit(review.review._id)} }>수정</button>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            ))}
+                        </table>
+                    </div>
+                </div>
+            </Col>
+        </div>
     </div>
     )
 }

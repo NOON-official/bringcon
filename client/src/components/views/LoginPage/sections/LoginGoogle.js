ee35907200369e7e72ec568eb8fab974600fed75
YeonSeong-Lee <dltjddus3@gmail.com>
2021-10-01 18:33:19 +0900

    add law

--- client/src/components/views/LoginPage/sections/LoginGoogle.js
+++ client/src/components/views/LoginPage/sections/LoginGoogle.js
@@ -6,7 +6,7 @@ import { useDispatch } from "react-redux";
 import { withRouter } from "react-router-dom";
 import axios from "axios";
 import { error } from "jquery";
-import './Login.css';
+import "./Login.css";
 
 function LoginGoogle(props) {
   const dispatch = useDispatch();
@@ -14,27 +14,13 @@ function LoginGoogle(props) {
   const clientId =
     "868725939307-s3dblc74bco886t7kmimukn9rodb1vrj.apps.googleusercontent.com";
 
-  const SCOPE = "https://www.googleapis.com/auth/youtube.readonly";
-
   async function onSuccess(res) {
-    const getChannelId = await axios
-      .get(
-        `https://www.googleapis.com/youtube/v3/channels?part=id&mine=true&access_token=${res.accessToken}`
-      )
-      .then((response) => {
-        return response.data.items;
-      })
-      .catch((err) => console.log(err));
-
-    const channelId = getChannelId;
-
     const profile = res.getBasicProfile();
 
     const dataToSubmit = {
       email: profile.getEmail(),
       image: profile.getImageUrl(),
       name: profile.getName(),
-      channelId: channelId,
     };
 
     dispatch(registerUser(dataToSubmit)).then((response) => {
@@ -65,7 +51,6 @@ function LoginGoogle(props) {
         onSuccess={onSuccess}
         onFailure={onFailure}
         cookiePolicy={"single_host_origin"}
-        scope={SCOPE}
       />
     </div>
   );
